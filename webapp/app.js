99e7ad086f052e045ce480460d992e3eb24f3792
sarfaraz <sarfaraz@localhost.localdomain>
2026-04-18 18:52:57 +0530

    added webapp code

---
+++ webapp/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World from Node.js 🚀");
+});
+
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "OK" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
